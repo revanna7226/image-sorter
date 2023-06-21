285e7dea18adf610d04541888ff554e41c9e3539
Revs <revanna.rsn@gmail.com>
2023-06-21 20:51:00 +0530

    Integrated VGG 2.0.12

---
+++ src/main/java/com/revs/sorter/ImageSorterApplication.java
@@ -0,0 +1,13 @@
+package com.revs.sorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSorterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revs/sorter/ImageSorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revs.sorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
